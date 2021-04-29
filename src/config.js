0d05525e9a27b82eba8f4c2ab6de2a9a7d3fa788
Ian Nascimento <ian.nascimento2011@hotmail.com>
2021-04-29 20:22:45 -0300

    add settings storages

--- src/config.js
+++ src/config.js
@@ -1,20 +1,44 @@
-const config = {
+const dev = {
     STRIPE_KEY: "pk_test_51IkfIqKEKi30yFz1lYpVS63DIc0eEN2wWjELtv3DO3QdBqfCiJskXq6sSopwBd251TT05HE7HbVml9Qyd8zhxHUo00rzp6kgyr",
-    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-east-1",
-        BUCKET: "notes-api-project",
+        BUCKET: "notes-api-dev-serverlessdeploymentbucket-48gbgg0afzr8"
     },
     apiGateway: {
         REGION: "us-east-1",
-        URL: "https://udll71zwii.execute-api.us-east-1.amazonaws.com/dev",
+        URL: "https://udll71zwii.execute-api.us-east-1.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-east-1",
-        USER_POOL_ID: "us-east-1_Y6E5BRycY",
-        APP_CLIENT_ID: "2cg02ang063ugqci7p8hgbtgc8",
-        IDENTITY_POOL_ID: "us-east-1:ae45bbb2-5e61-4ee3-afab-5619a3d102da",
+        USER_POOL_ID: "us-east-1_TrPVJgPk9",
+        APP_CLIENT_ID: "680gc4eqhunke0j9pm0v7g71gg",
+        IDENTITY_POOL_ID: "us-east-1:e1b21329-7b33-458d-8398-2a356d629269"
+    }
+};
+
+const prod = {
+    STRIPE_KEY: "pk_test_51IkfIqKEKi30yFz1lYpVS63DIc0eEN2wWjELtv3DO3QdBqfCiJskXq6sSopwBd251TT05HE7HbVml9Qyd8zhxHUo00rzp6kgyr",
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-api-prod-serverlessdeploymentbucket-dt76v34mqmrs"
     },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://x9vbmkx3td.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_8tbjH6xuR",
+        APP_CLIENT_ID: "1nrt3t8er3ps8iqr377va96icn",
+        IDENTITY_POOL_ID: "us-east-1:0e94b722-5c00-4234-9358-8005e8ab878e"
+    }
+};
+
+const config = {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    // Default to dev if not set
+    ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
 };
 
 export default config;
